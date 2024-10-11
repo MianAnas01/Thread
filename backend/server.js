4e68f521046dcbbf2c57699f6af6b8701bcad6af
M Anas <manas4808294@gmail.com>
2024-10-12 01:33:55 +0500

    Sign up done

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectdb.js";
-
+import cookieParser from "cookie-parser";
+import userRoutes from "../backend/routes/userRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse json data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
